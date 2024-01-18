ab2f37b32f448671d60d34c9c5056ce62c532453
p2glet <vviian.2@gmail.com>
2024-01-18 16:46:43 +0900

    [FEAT] : Created Components in WireViewController

--- ToWire/WireViewController.swift
+++ ToWire/WireViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class WireViewController: UIViewController {
+    // Components
+    private let titleLabel = CustomLabel(text: "환율 계산", font: Typography.title.font)
+    
+    private let wireTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+    
+    private let resultLabel = CustomLabel(text: "수취금액", font: Typography.largeContent.font)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
     }
-
-
 }
 
